b103213c02655c2006cbbb14f6ea909cad07793b
yuhanyuhan <yuhanlian@gmail.com>
2018-08-12 20:19:30 +0800

    performed new exercises

--- exercise-4.js
+++ exercise-4.js
@@ -1,9 +1,17 @@
 // 1. Create an array `names` with at least 3 elements: `'david'`, `'gordon'`, and
 //    `'sahil'` and log it to the console
+const names = ['david', 'gordan', 'sahil','yu','han']; 
+
 
 
 // 2. Log the value of the third element in `names` to the console
-
+console.log("before: "+names[2]);
+console.log(names.length); 
+names.length = 1; 
+console.log(names.length); 
+console.log(names)
+console.log(names[4]);
+console.log(names[3]);
 
 // 3. Log the value of the `names` array's `length` property to the console
 
@@ -30,10 +38,26 @@
 // 11. Change the `textContent` property of the _second_ `p` tag on the page to
 //    `"JavaScript was here!"`
 
+// console.log(document.querySelectorAll('p'))
+// console.log(document.querySelector('p'[1]))
+// document.querySelector('p').splice(1,0,'"JavaScript was here!"'); 
+// console.log(document.querySelector('p'[1]))
+
+
+const paragraph = document.querySelectorAll('p')
+const p = paragraph[1]
+p.textContent = '"JavaScript was here!"'; 
+
+
 
 // 12. Convert the `names` array to a string, with each element separated by a
 //    '<br>'. Store it in a new variable called `namesString` e.g. if `names` is
 //    `['harry', 'ron', 'dean']` then `namesString` will be `"harry<br>ron<br>dean"`
+
+
+let
+ namesString = paragraph.join('<br>');
+console.log(namesString)
 
 
 // 13. Change the `innerHTML` property of the _first_ `p` tag on the page to
--- exercise-5.js
+++ exercise-5.js
@@ -1,14 +1,23 @@
 // 1. Create a new array `shoppingList` with a few string elements
-
+const shoppingList = ['teeth', 'eyes', 'skull']
 
 // 2. Using `forEach`, log every element of the `shoppingList` array to the console
-
+shoppingList.forEach(e => console.log(e))
 
 // 3. For each element in the `shoppingList` array, create an `<li>` node and
 //    append it to the `<ul>` element on the page (use `document.createElement`
 //    and `.appendChild`)
 
+const appendList = (a) => {
+    const childList = document.createElement('li')
+    const list = document.querySelector('ul').appendChild(childList);
+    childList.textContent = a; 
+}
 
+shoppingList.forEach(appendList);  
+ 
+
+console.log(appendList);
 // 4. Create a new array `numbers` with a few numeric elements
 
 
